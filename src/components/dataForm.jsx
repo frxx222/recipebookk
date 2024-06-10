c269e273614ab2ce9e6116071ccc47d60f67cce6
frxx222 <jayveeenobay040@gmail.com>
2024-06-10 17:46:35 +0800

    Updated dependencies and resolved conflicts

--- src/components/dataForm.jsx
+++ src/components/dataForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navbar from "./navbar";
-import MyFavorite from "../pages/myFavorite";
+import MyFavorite from "../../pages/myFavorite";
 import './dataForm.css'
 
 const API_URL = 'https://aesthetic-elf-6dbaed.netlify.app/.netlify/functions/api/';
